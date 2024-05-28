01e5e32e0a3b697428d6916b9e134ffcebb42216
Sergei Barsukov <sergforweb@gmail.com>
2024-05-27 20:46:28 -0400

    add styles to carousel and section of pastor

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Head from "next/head";
 import BlogCard from "../components/BlogCard";
 import { motion } from "framer-motion";
 import Link from "next/link";
+import Image from 'next/image';
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
 
@@ -103,78 +104,148 @@ const Blog = ({ blogs }) => {
             </div>
             <div className="row">
               <div className="col-lg-6 text-white">
-                Photo
+              <Image
+                src="/img/pastor.jpg"
+                alt="My Image"
+                width={500}
+                height={500}
+              />
               </div>
               <div className="col-lg-6">
-                <p>
-                he vision for VOUS Church emerged out of the weekly gathering of The Rendezvous in Miami Gardens. The mission is to create a church in the heart of Miami that would be a home to all ages and represent the diversity of our beautiful city.
+                <p className="d-block">
+                Our church family is a beacon of hope in our community.  We are a place where people can find solace, connection, and spiritual growth.  But a church shouldn't be stagnant.  It should be a vibrant, ever-evolving organism, guided by a clear vision from God.
+                </p>
 
-                Our lead pastors, Rich and DawnCheré Wilkerson, have a heart to reach our city and beyond with the life-transforming message of Jesus. They believe in the next generation and value the wisdom and guidance of those that have gone before us.
+                <p className="d-block mt-4">
+                My vision for our church is one of inclusivity, outreach, and unwavering faith.
+                </p>
+
+                <p className="d-block mt-4">
+                This vision is not mine alone.  It is a vision that I believe God has placed on all of our hearts.  Together, through prayer, dedication, and commitment, we can turn this vision into reality.
                 </p>
               </div>
             </div>
           </div>
             
         </div>
-        <Carousel
-          additionalTransfrom={0}
-          arrows
-          autoPlaySpeed={3000}
-          centerMode={false}
-          className=""
-          containerClass="container"
-          dotListClass=""
-          draggable
-          focusOnSelect={false}
-          infinite
-          itemClass=""
-          keyBoardControl
-          minimumTouchDrag={80}
-          partialVisible
-          pauseOnHover
-          renderArrowsWhenDisabled={false}
-          renderButtonGroupOutside={false}
-          renderDotsOutside={false}
-          responsive={{
-            desktop: {
-              breakpoint: {
-                max: 3000,
-                min: 1024
+        <div className="bg-white py-12">
+          <div className="spacer">
+            <div className="container-fluid">
+              <div className="row">
+                <div className="col-lg-12">
+                  <h2 className="text-4xl pb-4 text-black">The latest at IRC</h2>
+                </div>
+              </div>
+            </div>
+          </div>
+          <Carousel
+            additionalTransfrom={0}
+            arrows
+            autoPlaySpeed={3000}
+            centerMode={false}
+            className="pt-12"
+            containerClass="container"
+            dotListClass=""
+            draggable
+            focusOnSelect={false}
+            infinite
+            itemClass=""
+            keyBoardControl
+            minimumTouchDrag={80}
+            partialVisible
+            pauseOnHover
+            renderArrowsWhenDisabled={false}
+            renderButtonGroupOutside={false}
+            renderDotsOutside={false}
+            responsive={{
+              desktop: {
+                breakpoint: {
+                  max: 3000,
+                  min: 1024
+                },
+                items: 3,
+                partialVisibilityGutter: 40
               },
-              items: 3,
-              partialVisibilityGutter: 40
-            },
-            mobile: {
-              breakpoint: {
-                max: 464,
-                min: 0
+              mobile: {
+                breakpoint: {
+                  max: 464,
+                  min: 0
+                },
+                items: 1,
+                partialVisibilityGutter: 30
               },
-              items: 1,
-              partialVisibilityGutter: 30
-            },
-            tablet: {
-              breakpoint: {
-                max: 1024,
-                min: 464
-              },
-              items: 2,
-              partialVisibilityGutter: 30
-            }
-          }}
-          rewind={false}
-          rewindWithAnimation={false}
-          rtl={false}
-          shouldResetAutoplay
-          showDots={false}
-          sliderClass=""
-          slidesToSlide={1}
-          swipeable
-        >
-          <div>Item 1</div>
-          <div>Item 2</div>
-          <div>Item 3</div>
-          <div>Item 4</div>
-        </Carousel>
+              tablet: {
+                breakpoint: {
+                  max: 1024,
+                  min: 464
+                },
+                items: 2,
+                partialVisibilityGutter: 30
+              }
+            }}
+            rewind={false}
+            rewindWithAnimation={false}
+            rtl={false}
+            shouldResetAutoplay
+            showDots={false}
+            sliderClass=""
+            slidesToSlide={1}
+            swipeable
+          >
+            <div>
+              <div className="latest-vous-card">
+                <img loading="eager" src="https://assets-global.website-files.com/5f26e92b7cfa446572ec9d92/6612a23be1b7d400ed93ef96_web.jpg" alt="" sizes="350px" srcset="https://assets-global.website-files.com/5f26e92b7cfa446572ec9d92/6612a23be1b7d400ed93ef96_web-p-500.jpg 500w, https://assets-global.website-files.com/5f26e92b7cfa446572ec9d92/6612a23be1b7d400ed93ef96_web.jpg 564w" className="_100-img" />
+                <div className="latest-vous-text-wrap">
+                  <div className="overline-meta-10">
+                    COLLECTION
+                  </div>
+                  <h4 className="text-black">
+                    Mature Church
+                  </h4>
+                </div>
+              </div>
+            </div>
+            <div>
+              <div className="latest-vous-card">
+                <img loading="eager" src="https://assets-global.website-files.com/5f26e92b7cfa446572ec9d92/6612a23be1b7d400ed93ef96_web.jpg" alt="" sizes="350px" srcset="https://assets-global.website-files.com/5f26e92b7cfa446572ec9d92/6612a23be1b7d400ed93ef96_web-p-500.jpg 500w, https://assets-global.website-files.com/5f26e92b7cfa446572ec9d92/6612a23be1b7d400ed93ef96_web.jpg 564w" className="_100-img" />
+                <div className="latest-vous-text-wrap">
+                  <div className="overline-meta-10">
+                    COLLECTION
+                  </div>
+                  <h4 className="text-black">
+                    Mature Church
+                  </h4>
+                </div>
+              </div>
+            </div>
+            <div>
+              <div className="latest-vous-card">
+                <img loading="eager" src="https://assets-global.website-files.com/5f26e92b7cfa446572ec9d92/6612a23be1b7d400ed93ef96_web.jpg" alt="" sizes="350px" srcset="https://assets-global.website-files.com/5f26e92b7cfa446572ec9d92/6612a23be1b7d400ed93ef96_web-p-500.jpg 500w, https://assets-global.website-files.com/5f26e92b7cfa446572ec9d92/6612a23be1b7d400ed93ef96_web.jpg 564w" className="_100-img" />
+                <div className="latest-vous-text-wrap">
+                  <div className="overline-meta-10">
+                    COLLECTION
+                  </div>
+                  <h4 className="text-black">
+                    Mature Church
+                  </h4>
+                </div>
+              </div>
+            </div>
+            <div>
+              <div className="latest-vous-card">
+                <img loading="eager" src="https://assets-global.website-files.com/5f26e92b7cfa446572ec9d92/6612a23be1b7d400ed93ef96_web.jpg" alt="" sizes="350px" srcset="https://assets-global.website-files.com/5f26e92b7cfa446572ec9d92/6612a23be1b7d400ed93ef96_web-p-500.jpg 500w, https://assets-global.website-files.com/5f26e92b7cfa446572ec9d92/6612a23be1b7d400ed93ef96_web.jpg 564w" className="_100-img" />
+                <div className="latest-vous-text-wrap">
+                  <div className="overline-meta-10">
+                    COLLECTION
+                  </div>
+                  <h4 className="text-black">
+                    Mature Church
+                  </h4>
+                </div>
+              </div>
+            </div>
+          </Carousel>
+        </div>
       </main>
     </>
   );
